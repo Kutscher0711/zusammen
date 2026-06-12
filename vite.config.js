1bd71d834b6a6d883f79912a5d52e0229b3aab7c
Marco Schmid <marcoschmid@MacBookPro.fritz.box>
2026-06-12 17:19:49 +0200

    Switch to Vercel deployment, fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/zusammen/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
@@ -16,8 +16,8 @@ export default defineConfig({
         theme_color: '#F4F5F7',
         background_color: '#F4F5F7',
         display: 'standalone',
-        start_url: '/zusammen/',
-        scope: '/zusammen/',
+        start_url: '/',
+        scope: '/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
